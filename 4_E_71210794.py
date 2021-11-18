300b9000559225b91e87848a61a57923a8044ef7
EvanderGab <evander.gabriel@ti.ukdw.ac.id>
2021-11-18 19:09:28 +0700

    Tambah Soal 4_E_71210794.py

---
+++ 4_E_71210794.py
@@ -0,0 +1,34 @@
+a=int(input("Masukan bilangan 1 : "))
+b=int(input("Masukan bilangan 2 : "))
+c=int(input("Masukan bilangan 3 : "))
+#-3,4,-9
+
+if      a > b:
+        x = a
+        a = b
+        b = x    
+
+
+if      b > c:
+        y = b
+        b = c
+        c = y
+
+    
+
+if       a > b:
+        z = a
+        a = b
+        b = z
+
+
+
+
+print(a,b,c)
+
+
+#bilangan = bil1, bil2, bil3
+#bilangan1=','.join(bilangan)
+#bilangan2= bilangan1.split()
+
+#print(bilangan2)
